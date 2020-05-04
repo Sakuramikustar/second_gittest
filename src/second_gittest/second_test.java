95b6423f8a581e4ced7c4b37a284273f46009a72
sakuramikustar <3514479815@qq.com>
2020-05-04 18:00:54 +0800

    第二次且为第二种类提交

---
+++ src/second_gittest/second_test.java
@@ -0,0 +1,12 @@
+package second_gittest;
+
+/**
+ * @Author zhangguozheng
+ * @Version 1.0
+ * @hm miku
+ */
+public class second_test {
+    public static void main(String[] args) {
+        System.out.println("第二次测试");
+    }
+}
